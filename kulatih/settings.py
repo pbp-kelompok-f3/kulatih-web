a47ef73480070a36552da34a7ed34212b2f7b9f3
inoperatingsystem <julius.albert@cs.ui.ac.id>
2025-10-21 22:24:26 +0700

    Add pws deployment link to allowed hosts

--- kulatih/settings.py
+++ kulatih/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-2q=r)@c&pcc*o7im9a133fsui-jbp^nfsuj34zi2*s-5%z=8&#
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app", "julius-albert-kulatih.pbp.cs.ui.ac.id"]
 
 
 # Application definition
