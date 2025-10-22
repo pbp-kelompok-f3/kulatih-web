5669053cf47fcc9410108feab9aec1efe1eb86e0
inoperatingsystem <julius.albert@cs.ui.ac.id>
2025-10-23 01:21:45 +0700

    Add member and coach authentication and registration

--- kulatih/settings.py
+++ kulatih/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'kulatih.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
