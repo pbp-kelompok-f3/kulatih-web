62b3b8eb7580108072f988ad62c1b358dfd0971f
izziewizzle <izzati.mhr@gmail.com>
2025-10-23 18:23:32 +0700

    TAHAP2(5) navbar sticky

--- kulatih/settings.py
+++ kulatih/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'main',
     'forum',
     'users',
-    'tournaments'
+    'tournaments',
+    'community',
 ]
 
 MIDDLEWARE = [
@@ -142,11 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-import os
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
