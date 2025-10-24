20b81bba8802e39f1588533aa3428445fddaa29f
AliaArtanti <al.art295@gmail.com>
2025-10-24 11:35:14 +0700

    fix group community

--- kulatih/urls.py
+++ kulatih/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('account/', include('users.urls')),
     path('forum/', include('forum.urls')),
     path('tournament/', include('tournaments.urls')),
-    path('community/', include('community.urls')),,
     path('community/', include('community.urls')),
     path('booking/', include('booking.urls')),
     
