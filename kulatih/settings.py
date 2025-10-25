930fdb3d024f7eede0744b03ddeb420650b7e1f7
inoperatingsystem <julius.albert@cs.ui.ac.id>
2025-10-25 21:29:38 +0700

    Fix booking and add deployment link as csrf trusted origin

--- kulatih/settings.py
+++ kulatih/settings.py
@@ -33,6 +33,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app", "julius-albert-kulatih.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://julius-albert-kulatih.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
